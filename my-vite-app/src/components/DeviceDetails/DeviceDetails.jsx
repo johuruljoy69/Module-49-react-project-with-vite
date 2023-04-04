b27d0baa68cec69ee42ac5a490df557b2089e05c
johirjoy <joy.sd69@gmail.com>
2023-04-04 13:43:58 +0600

    first commit

---
+++ my-vite-app/src/components/DeviceDetails/DeviceDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeviceDetails = (props) => {
+    return (
+        <div>
+            <h5>It's Price is: {props.price} </h5>
+        </div>
+    );
+};
+
+export default DeviceDetails;
